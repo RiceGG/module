bca8607e9169d963be62035815e274ee3432292f
RiceGG <1948424125@qq.com>
2018-05-23 17:51:44 +0800

    first commit

---
+++ src/main/java/cn/com/taiji/module/App.java
@@ -0,0 +1,13 @@
+package cn.com.taiji.module;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
